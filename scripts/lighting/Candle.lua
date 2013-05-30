8cfc3f58d360ed4a894a5c8f7e7576768ca74d25
David Minnerly <dtreeLive@hotmail.com>
2013-05-30 14:42:26 -0400

    Update Candle script

--- scripts/lighting/Candle.lua
+++ scripts/lighting/Candle.lua
@@ -1,3 +1,17 @@
+
+--[[ TODO: (Do not include in final Script)
+   Get the time of day and adjust the brightness so it's dim
+   (almost unnoticable) in the daylight and extremely visible
+   at night.
+
+   Flickering light.
+
+   If I decide not to use a global script to manage
+   everything change the active variable to true,
+   and remove .Value from all changes to it.
+]]
+
+
 sp = script.Parent
 game_light = game.Lighting
 
@@ -8,28 +22,40 @@ function set_alight()
   local flame  = sp.Flame.Fire
   local range  = 15 -- The Range value of the light.
 
+  -- @TODO I need to make these while loops better.
   if active.Value == true then
-    -- If the light is active, extinguise it immediately.
+    -- If the light is active, extinguise it quickly.
     flame.Enabled = false
+
+    -- Fade out the light.
     while light.Range > 0 do wait()
       light.Range = light.Range - 5
     end
+
     active.Value = false
   else
     -- If the light is off, quickly, but smoothly, fade it in.
-    -- What's some better math to ensure it stops exactly at the predefined range?
+    -- @TODO What's some better math to ensure it stops exactly at the predefined range?
     flame.Enabled = true
-    while light.Range <= range do wait()
+
+    -- Fade in the light.
+    while light.Range < range do wait()
       light.Range = light.Range + 2.5
     end
+
     active.Value = true
   end
 end
+
+-- Connections go at the bottom for easy editing if there are multiples.
 sp.ClickDetector.MouseClick:connect(set_alight)
 
---[[
+
+--[[ Unused code. Keeping as a sort of archive.
+
 while true do wait(0.5)
   -- Quick test to see if the function can be used while this is looping.
+  -- Update: They can!
   words = {"Cats","Butts","Oranges","Rabbits","DEER HEAD"}
   for i = 1,#words do
     print(words[math.random(i)])
