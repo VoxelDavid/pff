29ffb537080828033fc66b91fced54db559a900c
David Minnerly <dtreeLive@hotmail.com>
2013-06-04 16:29:36 -0400

    Move wiki-esque content from GlobalScripts.lua to new Wiki page

--- scripts/GlobalScripts.lua
+++ scripts/GlobalScripts.lua
@@ -1,5 +1,5 @@
 --[===================================[--
-	@Description:
+	@Description
 		All of the children of this script are used throughout
 		the game by multiple objects.
 
@@ -10,13 +10,4 @@
 		It's not a perfect solution. Some times the scripts used to call
 		the global functions need to be edited, but overall this is much
 		better than copying a big function everywhere.
-
-	@Creating a Function:
-		repeat wait() until _G.config    -- Wait for the configuration file before doing anything else.
-
-		function _G.function_name()      -- Create the function.
-			-- Code
-		end
-
-		print("_G.function_name Loaded") -- Output to the console that the script is loaded.
 --]===================================]--
---
+++ scripts/README.md
@@ -0,0 +1,2 @@
+## Adding a new Function
+https://github.com/VoxelDavid/pff/wiki/Creating-a-new-Function
