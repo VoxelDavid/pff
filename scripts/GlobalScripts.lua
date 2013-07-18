b47f342ba7c712ad16abacac6cf3ed68ca84b736
David Minnerly <dtreeLive@hotmail.com>
2013-07-18 10:06:25 -0400

    Fix a minor typo

--- scripts/GlobalScripts.lua
+++ scripts/GlobalScripts.lua
@@ -5,7 +5,7 @@
 
     Without the functions being global they would have to be copied
     many times throughout the game and if a change is made, then all
-    scripts containging the function need to be updated.
+    scripts containing the function need to be updated.
 
     It's not a perfect solution. Some times the scripts used to call
     the global functions need to be edited, but overall this is much
