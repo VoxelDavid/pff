e3ecfac2afee8eea2e42ade68424312184cf3ead
David Minnerly <dtreeLive@hotmail.com>
2013-12-12 14:51:32 -0500

    Edit GLOBAL
    - Changed main comment block.
    - Added "Variables" and "Tables" sections.
    - Added semicolons at the end of statements.

--- src/GLOBAL.lua
+++ src/GLOBAL.lua
@@ -1,60 +1,49 @@
---[==[--
-  |--------------------------------------------------------------------------
-  | GLBOAL
-  |--------------------------------------------------------------------------
-  |
-  | This script contains all of the variables accessed throughout the game.
-  |
-  | All children of this script are Global Functions accessed by "call scripts"
-  |
-  | Keep orginized by:
-  | - Ordering tables alphabetically
-  | - Keeping variables of the same type (IE: Bool, Int, etc.) grouped together.
-  | - Putting singular variables above Tables.
-  |
-  |--------------------------------------------------------------------------
---]==]--
 
--- Game Version -- Keep this regularly updated!
-_G.version = "Alpha 0.1.0"
+--[[
+ ## GLOBAL
 
--- Loose Variables
-_G.config = script.Configuration -- Where all values accessed by Scripts are stored.
+ This script contains all of the variables accessed throughout the game. All
+ children of this script are Global Functions accessed by "call scripts".
+
+ View the documentation for more info.
+]]--
+
+-- ##
+-- ## Variables
+-- ##
+
+_G.version = "Alpha 0.1.0";        -- Game Version — Keep this regularly updated!
+_G.config = script.Configuration;  -- Where all values accessed by Scripts are stored.
+
+
+-- ##
+-- ## Tables
+-- ##
 
 -- Variables for all lighting related objects/scripts
 _G.lighting = {
-	brightness_when_day    = 0.3,  -- How bright a light will be in the day.
-	brightness_when_night  = 1,    -- How bright a light will be at night.
-	flicker_frequency      = 3,    -- How frequently a fire based light will flicker each second.
+	brightness_when_day   = 0.3,  -- How bright a light will be in the day.
+	brightness_when_night = 1,    -- How bright a light will be at night.
+	flicker_frequency     = 3,    -- How frequently a fire based light will flicker each second.
 
 	candle = {
-		range = 14,        -- Candle's PointLight range.
+		range       = 14,  -- Candle's PointLight range.
 		light_speed = 6,   -- How quickly the fire will be lit. Bigger number = slower transition.
 		ext_speed   = 3,   -- How quickly the fire will be extinguished. Bigger number = slower transition.
-	},
+	};
 
 	fireplace = {
-		range = 18,        -- Fireplace's PointLight range.
+		range       = 18,  -- Fireplace's PointLight range.
 		light_speed = 12,  -- How quickly the fire will be lit. Bigger number = slower transition.
 		ext_speed   = 3,   -- How quickly the fire will be extinguished. Bigger number = slower transition.
-	},
+	};
 
 	torch = {
-		range = 16,        -- Torch's PointLight range.
+		range       = 16,  -- Torch's PointLight range.
 		light_speed = 8,   -- How quickly the fire will be lit. Bigger number = slower transition.
 		ext_speed   = 3,   -- How quickly the fire will be extinguished. Bigger number = slower transition.
-	},
-}
-
--- Variables for the Day/Night time cycle.
-_G.time_cycle = {
-	enabled        = true,  -- If the Time Cycle should be enabled.
-	sky_ambiance   = true,  -- (Not implemented) If the Sky's ambiance should change for sunrise and sunset.
-	time_increment = 1,     -- The amount added to the current time every loop.
-	loop_speed     = 0.5,   -- How quickly the loop will iterate.
-	day_length     = 8,     -- (Not implemented) How many minutes it will take to go from sunrise to sunset.
-	night_length   = 4,     -- (Not implemented) How many minutes it will take for night to pass.
-}
+	};
+};
 
 -- Special Users
 _G.users = {
@@ -63,17 +52,18 @@ _G.users = {
 		"D3lego",
 		"MonsterBone",
 		"Player1",
-	},
+	};
 
 	-- Friends and other important people.
 	vips = {
 		"",
 		"",
 		"",
-	}
-}
+	};
+};
 
 -- Let other scripts know that the variables are ready for use.
-_G.ready = true
+_G.ready = true;
 
-print("Loaded "..script.Name)
+-- The script is loaded!
+print("Loaded "..script.Name);
