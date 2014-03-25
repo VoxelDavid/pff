9cb44cdcdee4dc499f16346bc8d7e6758162a8f0
David Minnerly <dtreeLive@hotmail.com>
2014-03-25 16:50:00 -0400

    Rewrite the Global file in preperation for module scripts.
    Rewrite doc block.
    Remove 'lighting' and 'users' tables.
    Set 'modules' and 'values' paths.

--- src/GLOBAL.lua
+++ src/GLOBAL.lua
@@ -1,69 +1,16 @@
-
 --[[
- GLOBAL
- ======
+ This script contains variables (constants, in this case) used by scripts
+ throughout the game.
 
- This script contains all of the variables accessed throughout the game. All
- children of this script are Global Functions accessed by "call scripts".
+ Children of this script are primarily composed of ModuleScripts that
+ get require()'d in 'call scripts' inside objects in the Workspace.
+]]
 
- View the documentation for more info.
-]]--
+_G.version = "Alpha 0.1.0" -- Game version - keep this regularly updated!
+_G.modules = script        -- Location of ModuleScripts
+_G.values  = script        -- Location of Values. Bool, string, int, etc.
 
--- ##
--- ## Variables
--- ##
-
-_G.version = "Alpha 0.1.0"        -- Game Version — Keep this regularly updated!
-_G.config = script.Configuration  -- Where all values accessed by Scripts are stored.
-
-
--- ##
--- ## Tables
--- ##
-
--- Variables for all lighting related objects/scripts
-_G.lighting = {
-	brightness_when_day   = 0.3,  -- How bright a light will be in the day.
-	brightness_when_night = 1,    -- How bright a light will be at night.
-	flicker_frequency     = 3,    -- How frequently a fire based light will flicker each second.
-
-	candle = {
-		range       = 14,  -- Candle's PointLight range.
-		light_speed = 6,   -- How quickly the fire will be lit. Bigger number = slower transition.
-		ext_speed   = 3,   -- How quickly the fire will be extinguished. Bigger number = slower transition.
-	},
-
-	fireplace = {
-		range       = 18,  -- Fireplace's PointLight range.
-		light_speed = 12,  -- How quickly the fire will be lit. Bigger number = slower transition.
-		ext_speed   = 3,   -- How quickly the fire will be extinguished. Bigger number = slower transition.
-	},
-
-	torch = {
-		range       = 16,  -- Torch's PointLight range.
-		light_speed = 8,   -- How quickly the fire will be lit. Bigger number = slower transition.
-		ext_speed   = 3,   -- How quickly the fire will be extinguished. Bigger number = slower transition.
-	},
-}
-
--- Special Users
-_G.users = {
-	-- Administrators
-	admins = {
-		"D3lego",
-		"MonsterBone",
-		"Player1",
-	},
-
-	-- Friends and other important people.
-	vips = {
-		"",
-		"",
-		"",
-	},
-}
-
--- Let other scripts know that the variables are ready for use.
+-- Alert other scripts that global values are ready for use.
 _G.ready = true
 
 -- The script is loaded!
