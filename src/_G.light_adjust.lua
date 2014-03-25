87818fb774b181569dc91a1f06372dfc5caabce7
David Minnerly <dtreeLive@hotmail.com>
2014-03-25 14:27:02 -0400

    Move scripts from folders up to the src/ directory.

--- src/_G.light_adjust.lua
+++ src/_G.light_adjust.lua
@@ -15,24 +15,24 @@
 
 repeat wait() until _G.ready
 
-function _G.light_adjust(light)
+function _G.light_adjust(light_source)
 	-- 1 = Sunrise, 2 = Day, 3 = Sunset, 4 = Night.
 	local time_of_day = _G.config.time_of_day
 
 	-- If the time is Sunrise or Day.
 	if time_of_day.Value == 1 or time_of_day.Value == 2 then
 		-- Fade out the brightness to the predefined brightness in the global config.
-		while light.Brightness > _G.lighting.brightness_when_day do
+		while light_source.Brightness > _G.lighting.brightness_when_day do
 			wait(0.1)
-			light.Brightness = light.Brightness - 0.03
+			light_source.Brightness = light_source.Brightness - 0.03
 		end
 
 	-- If the time is Sunset or Night.
 	elseif time_of_day.Value == 3 or time_of_Day.Value == 4 then
 		-- Fade in the brightness back to normal.
-		while light.Brightness < _G.lighting.brightness_when_night do
+		while light_source.Brightness < _G.lighting.brightness_when_night do
 			wait(0.1)
-			light.Brightness = light.Brightness + 0.03
+			light_source.Brightness = light_source.Brightness + 0.03
 		end
 	end
 end
