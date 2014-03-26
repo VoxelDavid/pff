9898f3ce7315390f1856026a67fbb75a17b5274a
David Minnerly <dtreeLive@hotmail.com>
2014-03-26 10:38:14 -0400

    Abstract functions in LightingHandler.lua, plus other edits.
    Rework how variables are asigned (no tables).
    Remove ChangeAmbiance(). It was just a placeholder, it can be recreated.
    Remove some comments.

--- src/LightingHandler.lua
+++ src/LightingHandler.lua
@@ -1,100 +1,57 @@
-
 --[[
- Lighting Handler
- ================
+  This script takes care of server-specific lighting tasks.
 
- This script takes care of lighting related tasks, such as the Day/Night cycle,
- setting a 'time of day' value for other scripts to hook into, so they can adjust
- things depending on the time, and changing the ambiance at sunrise and sunset.
-]]--
+  The main functionality consists of a time cycle, and creating a 'time of day'
+  value for other scripts to hook into.
+]]
+
+local timeIncrement = .75 -- The amount added on to the current time every recurrence.
+local loopSpeed = .25     -- How long the loop will wait() before adding timeIncrement.
+
+local lighting = game:GetService("Lighting")
+local currentTime = lighting:GetMinutesAfterMidnight()
 
 repeat wait() until _G.ready
 
--- Script Configuration
-local config = {
-    lighting_ambiance = true,  -- (Not implemented) If the ambiance should change for sunrise and sunset.
-    time_increment    = 1,     -- The amount added to the current time every loop.
-    loop_speed        = 0.5,   -- How quickly the loop will iterate.
-    day_length        = 480,   -- (Not implemented) How many seconds it will take to go from sunrise to sunset.
-    night_length      = 240,   -- (Not implemented) How many seconds it will take for night to pass.
-}
-
-local l = game:service("Lighting")
-
-
---[[
- Starts the main loop
-]]--
-function LightingHandler()
-    while wait(config.loop_speed) do
-        TimeCycle()
-        OutputTimeOfDay()
-    end
+function lightingLoop()
+  while wait(loopSpeed) do
+    outputTimeOfDay()
+    timeCycle()
+  end
 end
 
-
---[[
- Increments the current time by config.time_increment
-]]--
-function TimeCycle()
-    l:SetMinutesAfterMidnight(l:GetMinutesAfterMidnight() + config.time_increment)
+function timeCycle()
+  lighting:SetMinutesAfterMidnight(currentTime + timeIncrement)
 end
 
+function outputTimeOfDay()
+  -- Generate the TimeOfDay value if it doesn't already exist.
+  if not _G.values:FindFirstChild("TimeOfDay") then
+    local timeValue = Instance.new("StringValue", _G.values)
+    timeValue.Name = "TimeOfDay"
+  end
 
---[[
- Sets the _G.config.time_of_day value to a number between 1 and 4.
- 1 is Sunrise, 2 is Day, 3 is Sunset and 4 is Night.
+  local timeOfDay = _G.values.TimeOfDay
 
- Other scripts can then check this value and adjust based on the time.
-]]--
-function OutputTimeOfDay()
-    local time_of_day = _G.config.time_of_day
+  local sunrise = 360
+  local day     = sunrise + 60
+  local sunset  = 1060
+  local night   = sunset + 60
 
-    local sunrise_start = 360
-    local sunrise_end   = sunrise_start + 60
-    local sunset_start  = 1060
-    local sunset_end    = sunset_start + 60
-
-    -- Check the time against the 4 grouped variables above
-    -- and output the time of day accordingly.
-
-    -- Between sunrise_start and sunrise_end
-    if l:GetMinutesAfterMidnight() > sunrise_start and l:GetMinutesAfterMidnight() < sunrise_end then
-        time_of_day.Value = 1 -- Sunrise
-
-    -- Between sunrise_end and sunset_start
-    elseif l:GetMinutesAfterMidnight() > sunrise_end and l:GetMinutesAfterMidnight() < sunset_start then
-        time_of_day.Value = 2 -- Day
-
-    -- Between sunset_start and sunset_end
-    elseif l:GetMinutesAfterMidnight() > sunset_start and l:GetMinutesAfterMidnight() < sunset_end then
-        time_of_day.Value = 3 -- Sunset
-
-    -- Between sunset_end and sunrise_start
-    elseif l:GetMinutesAfterMidnight() > sunset_end and l:GetMinutesAfterMidnight() < sunrise_start then
-        -- For some reason time_of_day is never getting set to 4.
-        -- I'm not sure why that would be, the condition looks like it would work fine.
-        time_of_day.Value = 4 -- Night
-
-    end
+  -- Check the time against the four grouped variables above and
+  -- output the time of day acordingly.
+  if currentTime > sunrise and currentTime < day then
+    timeOfDay.Value = "sunrise"
+  elseif currentTime > day and currentTime < sunset then
+    timeOfDay.Value = "day"
+  elseif currentTime > sunset and currentTime < night then
+    timeOfDay.Value = "sunset"
+  elseif currentTime > night and currentTime < sunrise then
+    timeOfDay.Value = "night"
+  end
 end
 
-
---[[
- Coming later. This will change the Sky's ambiance at
- Sunrise and Sunset.
-]]--
-function ChangeAmbiance()
-    if config.lighting_ambiance == false then
-        return
-    end
-
-    -- code
-end
-
-
--- The script is loaded!
 print("Loaded " .. script.Name)
 
 -- Run the function to get the loop started.
-LightingHandler()
+lightingLoop()
