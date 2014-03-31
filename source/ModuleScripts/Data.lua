b27821d3d1a54b2b7849653eb2059270043cbbe4
David Minnerly <dtreeLive@hotmail.com>
2014-03-31 11:23:18 -0400

    Implement scopes as an optional last argument to Data functions.

--- source/ModuleScripts/Data.lua
+++ source/ModuleScripts/Data.lua
@@ -11,24 +11,32 @@
     data.delete("Server", "version")
     print(data.get("Server", "version")) -- "deleted"
 
-  ToDo:
-  - Implement Scopes
+  Namespaces/Scopes can be set as an optional last argument:
+
+    data.put("Server", "version", "v0.1.0-alpha.1", "customNamespace")
+    print(data.get("Server", "version")) -- "v0.1.0" (Assuming you used the earlier example, otherwise no value will appear.)
+    print(data.get("Server", "version", "customNamespace")) -- "v0.1.0-alpha.1"
 ]]
 
 local DataStore = game:GetService("DataStoreService")
+
+local globalScope = "global" -- default scope
 
 local Data = {
   -- Functions are named after the HTTP request methods. :)
 
   -- Creates a new key/value pair in the data store.
-  post = function(storeName, key, value)
-    local dataStore = DataStore:GetDataStore(storeName)
+  post = function(storeName, key, value, scope)
+    local scope = scope or globalScope
+    local dataStore = DataStore:GetDataStore(storeName, scope)
+
     dataStore:SetAsync(key, value)
   end,
 
   -- Updates/creates a key/value pair in the data store.
-  put = function(storeName, key, value)
-    local dataStore = DataStore:GetDataStore(storeName)
+  put = function(storeName, key, value, scope)
+    local scope = scope or globalScope
+    local dataStore = DataStore:GetDataStore(storeName, scope)
 
     function updateCallback()
       return value
@@ -38,15 +46,19 @@ local Data = {
   end,
 
   -- Returns the key from the data store for use in the game.
-  get = function(storeName, key)
-    local dataStore = DataStore:GetDataStore(storeName)
+  get = function(storeName, key, scope)
+    local scope = scope or globalScope
+    local dataStore = DataStore:GetDataStore(storeName, scope)
+
     return dataStore:GetAsync(key)
   end,
 
   -- Sets the key's value to the string "deleted" to indicate
   -- that it's been removed.
-  delete = function(storeName, key)
-    post(storeName, key, "deleted")
+  delete = function(storeName, key, scope)
+    local scope = scope or globalScope
+
+    post(storeName, key, "deleted", scope)
   end
 }
 
