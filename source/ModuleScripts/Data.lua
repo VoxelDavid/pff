a4aed5b1785814d1be7ff28a2a447683c4043bb6
David Minnerly <dtreeLive@hotmail.com>
2014-03-31 11:16:44 -0400

    Lowercase all functions in the data module.

--- source/ModuleScripts/Data.lua
+++ source/ModuleScripts/Data.lua
@@ -5,11 +5,11 @@
 
     local data = require(path.to.module)
 
-    data.PUT("Server", "version", "v0.1.0")
-    print(data.GET("Server", "version")) -- "v0.1.0"
+    data.put("Server", "version", "v0.1.0")
+    print(data.get("Server", "version")) -- "v0.1.0"
 
-    data.DELETE("Server", "version")
-    print(data.GET("Server", "version")) -- "deleted"
+    data.delete("Server", "version")
+    print(data.get("Server", "version")) -- "deleted"
 
   ToDo:
   - Implement Scopes
@@ -21,13 +21,13 @@ local Data = {
   -- Functions are named after the HTTP request methods. :)
 
   -- Creates a new key/value pair in the data store.
-  POST = function(storeName, key, value)
+  post = function(storeName, key, value)
     local dataStore = DataStore:GetDataStore(storeName)
     dataStore:SetAsync(key, value)
   end,
 
   -- Updates/creates a key/value pair in the data store.
-  PUT = function(storeName, key, value)
+  put = function(storeName, key, value)
     local dataStore = DataStore:GetDataStore(storeName)
 
     function updateCallback()
@@ -38,14 +38,14 @@ local Data = {
   end,
 
   -- Returns the key from the data store for use in the game.
-  GET = function(storeName, key)
+  get = function(storeName, key)
     local dataStore = DataStore:GetDataStore(storeName)
     return dataStore:GetAsync(key)
   end,
 
   -- Sets the key's value to the string "deleted" to indicate
   -- that it's been removed.
-  DELETE = function(storeName, key)
+  delete = function(storeName, key)
     post(storeName, key, "deleted")
   end
 }
