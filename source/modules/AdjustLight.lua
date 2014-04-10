3cf7919a41ca3b2ffb1674b59c54540a859c78ca
David Minnerly <dtreeLive@hotmail.com>
2014-04-09 20:54:55 -0400

    Add comment to the adjustBrightness 'if' block.

--- source/modules/AdjustLight.lua
+++ source/modules/AdjustLight.lua
@@ -1,6 +1,6 @@
 --[[
-  This script simply hooks into the TimeOfDay global value and fades
-  the brightness in or out depending on the value.
+  This script simply hooks into the TimeOfDay global value and fades the
+  brightness in or out depending on the value.
 --]]
 
 local brightnessWhenDay = .3
@@ -11,6 +11,11 @@ repeat wait(.1) until _G.TimeOfDay
 function adjustBrightness(lightSource)
   local timeOfDay = _G.TimeOfDay
 
+  --[[
+    'morning' and 'dusk' are included incase the game's time is set in a place
+    where it won't be triggered by 'dawn' or 'evening' for a while, fading out
+    the light at the start of the game.
+  --]]
   if timeOfDay.Value == "dawn" or timeOfDay.Value == "morning" then
     fadeOut(lightSource)
   elseif timeOfDay.Value == "evening" or timeOfDay.Value == "dusk" then
