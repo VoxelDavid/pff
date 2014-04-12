0a3fe33641384348c7b6fc6e4910a2846185d8ba
David Minnerly <dtreeLive@hotmail.com>
2014-04-11 21:42:59 -0400

    Add script for disabling Core gui elements.

---
+++ source/DisableCoreGuis.lua
@@ -0,0 +1,9 @@
+--[[
+  Disable core gui elements.
+--]]
+
+local gui = game.StarterGui
+
+gui:SetCoreGuiEnabled("PlayerList", false)
+gui:SetCoreGuiEnabled("Health", false)
+gui:SetCoreGuiEnabled("Backpack", false)
