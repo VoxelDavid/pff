2d56f10dca27c9d4e2e9c123e6f3223153e708e3
David Minnerly <dtreeLive@hotmail.com>
2014-04-15 16:17:26 -0400

    Restructure Data module.
    With this style I don't need to set 'self' as the first parameter.

--- source/modules/Data.lua
+++ source/modules/Data.lua
@@ -31,55 +31,52 @@ local DataStore = game:GetService("DataStoreService")
 
 local DEFAULT_SCOPE = "global"
 
-local Data = {
-  -- Functions are named after the HTTP request methods. :)
+local Data = {}
 
-  --[[
-    Parameters:
+-- Data methods are named after the HTTP request methods. :)
 
-    - self      {table}  Set automatically when the colon is used to call the function.
-    - storeName {string} The name of the DataStore you want to access.
-    - key       {string} The key name to be used with the value when inserting the data.
-    - value     {any}    Accepts any value (Other than 'nil') to be applied as the value for 'key'.
-    - scope     {string} Optional last argument to set the scope/namespace of the Data Store.
-  ]]
+--[[
+  string storeName The name of the DataStore you want to access.
+  string key       The key name to be used with the value when storing the data.
+  any    value     Accepts any value (other than 'nil') to be applied as the value for 'key'.
+  string scope     Optional last argument to set the scope/namespace of the DataStore.
+--]]
 
-  -- Creates a new key/value pair in the data store.
-  Post = function(self, storeName, key, value, scope)
-    local scope = scope or DEFAULT_SCOPE
-    local dataStore = DataStore:GetDataStore(storeName, scope)
+-- Creates a new key/value pair in the data store.
+function Data:Post(storeName, key, value, scope)
+  local scope = scope or DEFAULT_SCOPE
+  local dataStore = DataStore:GetDataStore(storeName, scope)
 
-    dataStore:SetAsync(key, value)
-  end,
+  dataStore:SetAsync(key, value)
+end
 
-  -- Updates a key/value pair in the data store and will create
-  -- the pair if not found.
-  Put = function(self, storeName, key, value, scope)
-    local scope = scope or DEFAULT_SCOPE
-    local dataStore = DataStore:GetDataStore(storeName, scope)
+-- Updates a key/value pair in the data store and will create the pair if not
+-- found.
+function Data:Put(storeName, key, value, scope)
+  local scope = scope or DEFAULT_SCOPE
+  local dataStore = DataStore:GetDataStore(storeName, scope)
 
-    function updateCallback()
-      return value
-    end
-
-    dataStore:UpdateAsync(key, updateCallback)
-  end,
-
-  -- Returns the key from the data store for use in the game.
-  Get = function(self, storeName, key, scope)
-    local scope = scope or DEFAULT_SCOPE
-    local dataStore = DataStore:GetDataStore(storeName, scope)
-
-    return dataStore:GetAsync(key)
-  end,
-
-  -- Sets the key's value to the string "deleted" to indicate
-  -- that it's been removed.
-  Delete = function(self, storeName, key, scope)
-    local scope = scope or DEFAULT_SCOPE
-
-    self:Post(storeName, key, "deleted", scope)
+  function updateCallback()
+    return value
   end
-}
+
+  dataStore:UpdateAsync(key, updateCallback)
+end
+
+-- Returns the key from the data store for use in the game.
+function Data:Get(storeName, key, scope)
+  local scope = scope or DEFAULT_SCOPE
+  local dataStore = DataStore:GetDataStore(storeName, scope)
+
+  return dataStore:GetAsync(key)
+end
+
+-- Sets the key's value to the string "deleted" to indicate that it's been
+-- removed.
+function Data:Delete(storeName, key, scope)
+  local scope = scope or DEFAULT_SCOPE
+
+  self:Post(storeName, key, "deleted", scope)
+end
 
 return Data
