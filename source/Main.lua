1bcb05c889253f86a6b7707591c2a2e08eae088d
David Minnerly <dtreeLive@hotmail.com>
2014-05-04 14:05:17 -0400

    Rewrite Main's variable comment.

--- source/Main.lua
+++ source/Main.lua
@@ -1,9 +1,7 @@
---[[
-  This script contains variables (constants, in this case) used by scripts
-  throughout the game.
 
-  Children of this script are primarily composed of ModuleScripts that
-  get require()'d in 'call scripts' inside objects in the Workspace.
+--[[
+  Game properties -- these are appended to the globally accessible table for
+  server-side Scripts and Modules to hook in to.
 --]]
 
 _G.version = "v0.1.0" -- Game version - keep this regularly updated!
