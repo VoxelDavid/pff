4c7c0e159e979ef6795a41fb0d4650217c541cae
David Minnerly <dtreeLive@hotmail.com>
2014-05-21 01:02:02 -0400

    Set FilteringEnabled to 'true'.

--- source/Main.lua
+++ source/Main.lua
@@ -7,6 +7,21 @@
 _G.Version = "v0.1.0" -- Game version - keep this regularly updated!
 _G.Modules = game.ServerScriptService
 
+--[[
+  When enabled, this property prevents object replication from the client.
+
+  This means that if the client tries to add parts or manipulate the properties
+  of existing parts, the change will not propagate to the server or other
+  players.
+
+  As a side effect, this also breaks the majority of Gear created by Roblox.
+
+  Reference:
+  - http://wiki.roblox.com/index.php?title=FilteringEnabled_(Property)
+  - https://medium.com/roblox-stories/11dd0729d138
+--]]
+Workspace.FilteringEnabled = true
+
 -- Alert other scripts that global values are ready for use.
 _G.Ready = true
 
